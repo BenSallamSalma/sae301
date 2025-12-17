5fffaef0f4dbe75b980a3df6b13d81639d8919c2
Vivianatn <vivian.antoine@hotmail.com>
2025-12-17 09:42:37 +0100

    Sauvegarde Vivian 6

---
+++ src/Entity/Connexion.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConnexionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ConnexionRepository::class)]
+class Connexion
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adresse_mail = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAdresseMail(): ?string
+    {
+        return $this->adresse_mail;
+    }
+
+    public function setAdresseMail(string $adresse_mail): static
+    {
+        $this->adresse_mail = $adresse_mail;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+}
